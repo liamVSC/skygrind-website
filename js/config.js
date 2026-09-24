582443e0fd0d093979dcaf7580d757665e47dc38
liamVSC <43729866+liamVSC@users.noreply.github.com>
2026-09-24 05:38:31 +0100

    Configure SkyGrind Discord link

--- js/config.js
+++ js/config.js
@@ -1,1 +1,1 @@
-window.SKYGRIND_CONFIG={SERVER_IP:"play.skygrind.online",STORE_URL:"",DISCORD_URL:"",API_BASE_URL:""};
+window.SKYGRIND_CONFIG={SERVER_IP:"play.skygrind.online",STORE_URL:"",DISCORD_URL:"https://discord.gg/PCXm9qY3BM",API_BASE_URL:""};
