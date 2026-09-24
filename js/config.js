9719413b72cc121d8e91a66ea60ab83dd07a579f
liamVSC <43729866+liamVSC@users.noreply.github.com>
2026-09-24 05:59:15 +0100

    Centralize SkyGrind website configuration

--- js/config.js
+++ js/config.js
@@ -1,1 +1,8 @@
-window.SKYGRIND_CONFIG={SERVER_IP:"play.skygrind.online",STORE_URL:"",DISCORD_URL:"https://discord.gg/PCXm9qY3BM",API_BASE_URL:""};
+window.SKYGRIND_CONFIG = {
+  SERVER_IP: "play.skygrind.online",
+  STORE_URL: "",
+  DISCORD_URL: "https://discord.gg/PCXm9qY3BM",
+  API_BASE_URL: "",
+  SERVER_PORT_JAVA: "",
+  SERVER_PORT_BEDROCK: ""
+};
